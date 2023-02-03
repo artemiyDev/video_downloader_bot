e69d3f6b6be1bf6332ba886e46020bc3e01f7565
Artemiy <artemiyshirko@gmail.com>
2023-02-03 15:47:55 +0300

    before deployment3

--- filters/users_filters.py
+++ filters/users_filters.py
@@ -11,19 +11,18 @@ class IsSubscriber(BoundFilter):
         if config.test or  not config.subscription_check:
             return True
         print(message)
-        language_code = callback["from"]["language_code"]
+        language_code = message["from"]["language_code"]
 
         if language_code == 'en' or language_code == 'ru':
             chat_for_check = f'@{str(config.chanel_for_subs_ru_eng)}'
         else:
             chat_for_check = f'@{str(config.chanel_for_subs_others)}'
-        chat_member_data = await bot.get_chat_member(chat_for_check, int(callback.message.chat.id))
+        chat_member_data = await bot.get_chat_member(chat_for_check, int(message.chat.id))
 
         if chat_member_data.status != types.ChatMemberStatus.LEFT:
             return True
         else:
-            await callback.message.answer(
+            await message.answer(
                 f'Чтобы далее пользоваться этой функцией вступите в наш канал {chat_for_check}')
-            await callback.answer()
             # print(chat_member_data)
             raise CancelHandler()
