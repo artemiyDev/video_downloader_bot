1583d3c5e20fa001b3c6e37de8b8cce2ec845fb7
root <root@355718-slava53.tmweb.ru>
2023-05-24 12:49:53 +0300

    service

--- download_services/youtube.py
+++ download_services/youtube.py
@@ -1,12 +1,30 @@
 import requests
 import os
+import re
+from pytube.exceptions import AgeRestrictedError
 from utils.wrappers import async_wrap
 from pytube import YouTube
 from pytube import innertube
-
-
+import string
+import time
+import glob
 @async_wrap
-def download_youtube_video(youtube_url):
+def download_youtube_video(youtube_url,message_chat_id):
+    def make_valid_filename(input_string):
+        # Remove invalid characters
+        valid_chars = "-_.() %s%s" % (re.escape(string.ascii_letters), re.escape(string.digits))
+        filename = re.sub(r"[^%s]" % valid_chars, "", input_string)
+
+        # Replace spaces with underscores
+        filename = filename.replace(" ", "_")
+
+        # Truncate filename if necessary
+        max_length = 255  # Maximum filename length (adjust as needed)
+        if len(filename) > max_length:
+            filename = filename[:max_length]
+
+        return filename
+
     innertube._cache_dir = '.'
     innertube._token_file = os.path.join(innertube._cache_dir, 'tokens.json')
     # streams = yt.streams.filter()
@@ -18,14 +36,30 @@ def download_youtube_video(youtube_url):
 
     youtube_obj = YouTube(youtube_url,use_oauth=True, allow_oauth_cache=True)
     thumb_url = youtube_obj.thumbnail_url.replace('sddefault.jpg', 'maxresdefault.jpg')
-    highest_res_video = youtube_obj.streams.get_highest_resolution()
-    def_filename = highest_res_video.default_filename.replace(' ', '_')
+
+
+    # try:
+    #     highest_res_video = youtube_obj.streams.get_highest_resolution()
+    #     def_filename = highest_res_video.default_filename.replace(' ', '_')
+    #     highest_res_video.download(output_path=f'download_services/temp/youtube', filename=def_filename)
+    #     thumb_file_path = f'download_services/temp/youtube/{def_filename.replace("mp4", "jpg")}'
+    #     resolution = int(highest_res_video.resolution.replace('p', ''))
+    # except AgeRestrictedError:
+    #     def_filename = make_valid_filename(youtube_obj.title)
+    #     os.system(f'yt-dlp {youtube_url} -o download_services/temp/youtube/{def_filename}.webm')
+    #     def_filename = def_filename+'.webm'
+    #     thumb_file_path = f'download_services/temp/youtube/{def_filename.replace("webm", "jpg")}'
+    #     resolution = 720
+
+    def_filename = f'{str(message_chat_id)}_{str(int(time.time()))}'
+    thumb_file_path = f'download_services/temp/youtube/{def_filename}_thumb.jpg'
+    os.system(f'yt-dlp {youtube_url} -o "download_services/temp/youtube/{def_filename}.%(ext)s"')
+    def_filename = glob.glob(f'download_services/temp/youtube/{def_filename}*')[0]
+    resolution = 720
 
     r = requests.get(thumb_url)
-    thumb_file_path = f'download_services/temp/youtube/{def_filename.replace("mp4", "jpg")}'
     with open(thumb_file_path, 'wb') as f:
         f.write(r.content)
 
-    highest_res_video.download(output_path=f'download_services/temp/youtube', filename=def_filename)
-    return f'download_services/temp/youtube/{def_filename}', thumb_file_path, int(
-        highest_res_video.resolution.replace('p', ''))
+
+    return def_filename, thumb_file_path, resolution
--- handlers/services/youtube.py
+++ handlers/services/youtube.py
@@ -5,7 +5,7 @@ import traceback
 from aiogram import types
 from aiogram.types import InputFile
 from aiogram.utils.exceptions import CantParseEntities
-
+from pytube.exceptions import RegexMatchError
 from data.config import DEVELOPER
 from download_services.youtube import download_youtube_video
 from filters.users_filters import check_subscription
@@ -24,37 +24,46 @@ async def echo(message: types.Message):
             await message.answer_video(video_from_db['tg_file_id'],caption=CAPTION, parse_mode='HTML')
         else:
             await message.answer('<b>Дождись загрузки...</b>', parse_mode='HTML')
-            video_file_path, thumb_file_path, resolution = await download_youtube_video(video_url)
-            video = InputFile(video_file_path)
-            thumb = InputFile(thumb_file_path)
-            for retry in range(12):
+            video_file_path, thumb_file_path, resolution = await download_youtube_video(video_url,message.chat.id)
+
+            for retry in range(3):
                 try:
+                    print(video_file_path)
+                    print(thumb_file_path)
+                    video = InputFile(video_file_path)
+                    thumb = InputFile(thumb_file_path)
                     message_data = await message.answer_video(video=video, parse_mode='HTML', supports_streaming=True, thumb=thumb,
                                                               height=resolution, width=(resolution / 9) * 16,caption=CAPTION)
+                    break
+                except FileNotFoundError:
+                    time.sleep(1)
                 except:
+                    traceback.print_exc()
                     time.sleep(1)
 
 
             file_id = message_data['video']['file_id']
 
             await write_youtube_to_db(video_url, file_id)
-            try:
-                os.remove(video_file_path)
-            except:
-                pass
-            try:
-                os.remove(thumb_file_path)
-            except:
-                pass
         if config.subscription_check:
             subscribed = await check_subscription(message)
             if not subscribed:
                 await dp.bot.send_message(message.chat.id, PROMO_MESSAGE, disable_web_page_preview=True,
                                           parse_mode='HTML')
         await increase_stat('youtube')
+    except RegexMatchError:
+        await message.answer('Неверная ссылка')
     except Exception as e:
         traceback.print_exc()
         root_logger.error('Error youtube download ' + str(video_url), exc_info=True)
+        try:
+            await dp.bot.send_message(DEVELOPER[0], 'Error youtube download')
+            await dp.bot.send_message(DEVELOPER[0], e)
+            await dp.bot.send_message(DEVELOPER[0], video_url)
+        except CantParseEntities:
+            pass
+        await message.answer('Произошла ошибка, попробуйте позже')
+    finally:
         try:
             os.remove(video_file_path)
         except:
@@ -63,10 +72,4 @@ async def echo(message: types.Message):
             os.remove(thumb_file_path)
         except:
             pass
-        try:
-            await dp.bot.send_message(DEVELOPER[0], 'Error youtube download')
-            await dp.bot.send_message(DEVELOPER[0], e)
-        except CantParseEntities:
-            pass
-        await message.answer('Произошла ошибка, попробуйте позже')
     await message.answer('Хочешь скачать еще? Просто пришли ссылку')
